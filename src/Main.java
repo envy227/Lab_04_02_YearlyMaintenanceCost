1f4004ac1b210569a8257a540da3c9383087ed58
khushi <khushi.devadiga227@gmail.com>
2025-09-16 11:31:10 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 150.0;
+        double winterCost = 200.0;
+        double summerCost = 300.0;
+        double fallCost = 150.0;
+
+        double yearlyTotal = springCost + winterCost + fallCost + summerCost;
+
+        System.out.println("Your total cost for spring season is $" + springCost);
+        System.out.println("Your total cost for winter season is $" + winterCost);
+        System.out.println("Your total cost for summer season is $" + summerCost);
+        System.out.println("Your total cost for fall season is $" + fallCost);
+        System.out.println("Your yearly maintenance cost is $" + yearlyTotal);
+    }
+
+
+}
+
+
